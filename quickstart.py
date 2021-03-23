466d519dde752b56d41f618663c0038554869b2b
sillymagic93 <75815872+sillymagic93@users.noreply.github.com>
2021-03-22 22:15:43 -0700

    Update quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -1,5 +1,7 @@
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
+from datetime import datetime
+import time
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 SERVICE_ACCOUNT_FILE = 'mykeys.json'
@@ -15,30 +17,60 @@ Webflow_ID = '19xtDhqs3H4_3iBGgfKwllwW2AxusEpu1F67cIWY37i4'
 Zoom_links_ID = '1MV-4ZjOjoa--HYHSvZrOraP4lvmWyxmjPaM4v0TeacM'
 
 
-def getinfo(lis, order):
+def getinfo(lis):
 
-    #order of performer
-    info = [str(order)]
+    #0.performer email
+    info = [lis[1]]
 
-    #name of performer
+    #1.name of performer
     first_name, last_name_letter = lis[2], lis[3][0]
     info.append(first_name + ' ' + last_name_letter + '.')
-    info.append(lis[10])
 
-    # performance piece
+    #2.performance type
+    performance_type = lis[9]
+    if performance_type.lower() == 'instrument':
+        performance_type = lis[10]
+    if lis[10].lower() == 'other': performance_type = 'Instrument/Other'
+    info.append(performance_type)
+
+    #3.performance piece
     title = lis[11]
-    if lis[13] != '': title += ' ' + lis[13]
-    if lis[14] != '': title += ' ' + lis[14]
+    if 'op' not in title.lower():
+        if lis[13] != '':
+            if 'op' not in lis[13].lower(): title += ' Op.' + lis[13]
+            else: title += ' ' + lis[13]
+    if 'no.' in title.lower() or 'mov' in title.lower() or 'mvt' in title.lower():
+        title += ''
+    else:
+        if lis[14] != '':
+            if 'no' in lis[14].lower() or 'mov' in lis[14].lower() \
+                    or 'mvt' in lis[14].lower(): title += ' ' + lis[14]
+            else: title += ' Mvt.' + lis[14]
     if lis[12] != '': title += ' in ' + lis[12]
     info.append(title)
 
-    #Composer Name
+    #4.Composer Name
     info.append(lis[15])
 
-    return info
+    #session number
+    ind = lis[8].index('Session')
+    ind_res = int(lis[8][ind+8])
+
+    #############################################
+    #not printed information: for comparison ONLY
+    #5.performer age
+    info.append(int(lis[6]))
+
+    #6.submission date/time
+    info.append(lis[0])
+    #############################################
+
+    print(info)
+
+    return info, ind_res
 
 
-def zoominfo(lis):
+def zoom_info(lis):
     #session number
     lis[1], lis[0] = lis[0], lis[1]
     lis[0] = 'Session ' + lis[0]
@@ -57,16 +89,26 @@ def zoominfo(lis):
     return lis
 
 
+def sort(lis):
+    for i in range(len(lis)-1):
+        for j in range(i+1, len(lis)):
+            if lis[i][5] > lis[j][5]:
+                lis[i], lis[j] = lis[j], lis[i]
+            elif lis[i][5] == lis[j][5]:
+                date_time1, date_time2 = lis[i][6], lis[j][6]
+                timestamp1 = time.mktime(datetime.strptime(date_time1, "%Y-%m-%d %H:%M:%S").timetuple())
+                timestamp2 = time.mktime(datetime.strptime(date_time2, "%Y-%m-%d %H:%M:%S").timetuple())
+                if timestamp1 > timestamp2: lis[i], lis[j] = lis[j], lis[i]
+
+
 def main():
-    """Shows basic usage of the Sheets API.
-    Prints values from a sample spreadsheet.
-    """
+
     service = build('sheets', 'v4', credentials=creds)
 
     # Call the Sheets API and get input
     sheet = service.spreadsheets()
     raw_input1 = sheet.values().get(spreadsheetId=Webflow_ID,
-                                    range="Sheet1!A2:X4").execute()
+                                    range="Sheet1!A3:W12").execute()
     performer_info = raw_input1.get('values', [])
 
     raw_input2 = sheet.values().get(spreadsheetId=Zoom_links_ID,
@@ -85,40 +127,62 @@ def main():
     """
 
     #initialize
-    order = 1
-    title_switch = True
-    res = []
-
-    if title_switch:
-        res.append(['Order', 'Performer First Name', 'Performance Type', 'Performance Piece Name', 'Composer'])\
-
-
-    # gather information for each performer & append to output 2d list
-    for i in range(len(performer_info)):
-        res.append(getinfo(performer_info[i], order))
-        order += 1
+    title = ['Email', 'Performer First Name', 'Performance Type', 'Performance Piece Name', 'Composer']
+    master_list = {i+1: [] for i in range(8)}
 
     #organize zoom links
     for i in range(len(zoom_links)):
-        zoominfo(zoom_links[i])
+        zoom_info(zoom_links[i])
 
-    # res.insert(1, [])
-    # print(res)
+
+    #gather information for each performer & append to master list -> dictionary
+    for i in range(len(performer_info)):
+        performer, ind = getinfo(performer_info[i])
+        master_list[ind].append(performer)
+
+    #sort performers within each session
+    for i in range(1, 9):
+        sort(master_list[i])
+
+    #add zoom links and titles, delete extra info
+
+    for i in range(1, 9):
+        for j in range(len(master_list[i])):
+            temp = master_list[i][j]
+            del temp[5:]
+        master_list[i].insert(0, zoom_links[i-1])
+        master_list[i].insert(1, title)
+
 
     #delete extra info
     # service.spreadsheets().values().batchClear(spreadsheetId=Output_ID,
     #                                            body={'ranges': "Sheet1!A1:F8"}).execute()
 
     #output to file
-    # service.spreadsheets().values().update(spreadsheetId=Output_ID, range="Sheet1!A1",
-    #                                        valueInputOption="USER_ENTERED", body={"values": zoom_links}).execute()
+    for i in range(1, 9):
 
+        service.spreadsheets().values().update(spreadsheetId=Output_ID, range="Session{}!A1".format(i),
+                                               valueInputOption="USER_ENTERED",
+                                               body={"values": master_list[i]}).execute()
+    print("done")
 
-
-    """todo:
-    sort people by session
-    organize all into
-    """
 
 if __name__ == '__main__':
     main()
+
+#     1.print all in one sheet
+#     sort people by session
+#     organize all performers into different sessions based on their session number
+#
+#     ***add performers' emails with performer information
+#
+#     2. put all sessions in 8 different tabs
+#
+#     3. in each session, sort based on age
+#         if same age: sort base on time of registration
+#
+#     4. (in the future) automatically create zoom meetings
+#
+#     5. try test data
+#
+#
